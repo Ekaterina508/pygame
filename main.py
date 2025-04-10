4c9cb21922fea289768a164b09b22f3e965d10f0
Ekaterina508 <eralash13102007@yandex.ru>
2025-04-10 18:52:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-import pygame
+import pygame # импортируем библиотеку pygame
 
 class Food():
-    def __init__(self, a, c, d):
-        self.image = pygame.image.load(a) # загрузка картинки
-        self.rect = self.image.get_rect() # получение прямоугольника от картинки
-        self.x = c
-        self.y = d
+    def __init__(self, name_image, x, y, width, height): # конструктор, создание свойств
+        self.image = pygame.image.load(name_image) # создание картинки, ЭТО СВОЙСТВО
+        self.rect = self.image.get_rect() # создание прямямоугольника по границам картинки, ЭТО СВОЙСТВО
+        self.rect.x = x # координата x для картинки, ЭТО СВОЙСТВО
+        self.rect.y = y # координата y для картинки, ЭТО СВОЙСТВО
 
-    def draw_image(self): # метод адресовки картинки
-        screen.blit(self.image, (self.x, self.y))
-
-fon = Food("кухня.jpg", 0, 0) # создание объекта класса
-fon1 = Food("R.png", 0, 0)
-pygame.init()# обязательная программа
-window_size=(600,600)#размеры окна
-screen=pygame.display.set_mode(window_size)#сделать экран с размерами
-pygame.display.set_caption("Undertale")
-clock = pygame.time.Clock()#фпс
+    def draw_image(self): # метод отрисовки картинки
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
-while True:#игровой цикл
-    fon.draw_image() # применение метода адресовки картинки к объекту картинки
-    fon1.draw_image()
-    clock.tick(40)#40фпс\с
-    for event in pygame.event.get(): # события
-        if event.type == pygame.QUIT: # усли нажали крест
+pygame.init()# обязательная команда
+window_size=(600,540)# размеры окна
+screen = pygame.display.set_mode(window_size)# создание экрана с размерами
+plate = Food("plate.png", 250, 450, 191, 56) # создание объекта класса Food
+kitchen = Food("kitchen.png", 0, 0, 814, 540) # создание объекта класса Food
+
+clock = pygame.time.Clock()# создание игрового таймера
+
+
+while True:# игровой цикл
+    clock.tick(40)# 40фпс\с
+    kitchen.draw_image() # применение метода отрисовки к объекту класса Food
+    plate.draw_image()# применение метода отрисовки к объекту класса Food
+    pygame.display.update() # обновление содержимого экрана
+    
+    for event in pygame.event.get(): # проходимся по событиям
+        if event.type == pygame.QUIT: # если нажали на крестик
             pygame.QUIT() # выход из игры
-    pygame.display.update()#обновление содержимого экрана
+    pygame.display.update()
+ 
 
